8a8c0e09ebf06fe19dfc8287ed53ea045037e7d5
lokendra3211023 <lokendra@apnaschoolapp.com>
2023-06-14 10:11:37 +0530

    Added Structure of frontend

---
+++ frontend/src/layout/Footer.jsx
@@ -0,0 +1,10 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <Box>
+      <Text>Footer Component</Text>
+    </Box>
+  )
+}
