ed9cd663e0d0fec78042dcdaf08fd55d4833c985
lokendra3211023 <lokendra@apnaschoolapp.com>
2023-06-18 13:02:42 +0530

    backend added

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 const { connection } = require("./db");
 const { userRouter } = require("./Routes/User.routes");
+const { PetRouter } = require("./Routes/Pet.routes");
+const { FavouiteRouter } = require("./Routes/Favourite.routes");
+const { ShelterRouter } = require("./Routes/Shelter.routes");
+
 const app = express();
+
 require("dotenv").config;
 
+app.use(cors());
 app.use(express.json());
 
+app.use("/shelters", ShelterRouter);
 app.use("/users", userRouter);
+app.use("/pets", PetRouter);
+app.use("/favourites", FavouiteRouter);
 
 app.listen(process.env.PORT, async (req, res) => {
   try {
